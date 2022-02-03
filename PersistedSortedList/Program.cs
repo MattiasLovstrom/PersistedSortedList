19f56fe5762f4ba899bb64998a007a159fbcb391
Lövström, Mattias <mattias.lovstrom@seb.se>
2022-02-03 23:17:47 +0100

    Aded perf test

--- PersistedSortedList/Program.cs
+++ PersistedSortedList/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json;
 
@@ -10,8 +12,40 @@ namespace PersistedSortedList
     {
         static void Main(string[] args)
         {
+            FunctionTest();
+            PerformenceTest();
+        }
+
+        private static void PerformenceTest()
+        {
             File.Delete("repository.db");
             File.Delete("repository.index");
+
+            //var index = new Index<TestObject>(object=>Value)
+            //Index.Add(new TestObject { Value = "H" })
+
+            var rnd = new Random();
+            var list = new PersistedSortedList<TestObject>("repository");
+            var count = 1000;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(new TestObject { Value = rnd.NextDouble().ToString() });
+            }
+            Console.Out.WriteLine($"Adding {count} took {stopwatch.ElapsedMilliseconds}ms");
+
+            //var found = list.Get(new TestObject { Value = "A" });
+        }
+
+        static void FunctionTest()
+        {
+            File.Delete("repository.db");
+            File.Delete("repository.index");
+
+            //var index = new Index<TestObject>(object=>Value)
+            //Index.Add(new TestObject { Value = "H" })
+
             var list = new PersistedSortedList<TestObject>("repository");
             list.Add(new TestObject { Value = "H" });
             list.Add(new TestObject { Value = "C" });
